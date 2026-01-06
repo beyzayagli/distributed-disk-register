981135ed5211ffad59c4daa5d40cbce51ef1ff1a
beyzayagli <151576985+beyzayagli@users.noreply.github.com>
2026-01-06 21:15:30 +0300

    mesajları diskte tutmaya basladık

--- distributed-disk-register/src/main/java/com/example/family/CommandParser.java
+++ distributed-disk-register/src/main/java/com/example/family/CommandParser.java
@@ -1,5 +1,6 @@
 package com.example.family;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,10 +14,28 @@ class SetCommand {
     }
     
     public String execute() {
-        CommandParser.messages.put(key, value);
-        return "OK";
+        try{
+            //messages klasoru yoksa olustur
+            File dir = new File("messages");
+            if (!dir.exists()){
+                dir.mkdir();
+            }
+
+            //Dosyaya yaz
+            try (java.io.FileWriter fw = new java.io.FileWriter("messages/" + key + ".msg")){
+                fw.write(value);
+            }
+
+            //RAM'e de koy
+            CommandParser.messages.put(key, value);
+            return "OK";
+        }catch (Exception e){
+            System.out.println("SET hatasi: " + e.getMessage());
+            return "ERROR";
+        }
+        }
     }
-}
+
 
 class GetCommand {
     public String key;
@@ -26,6 +45,18 @@ class GetCommand {
     }
     
     public String execute() {
+        try{
+            //once diskten oku
+            try (java.io.BufferedReader br = new java.io.BufferedReader( new java.io.FileReader("messages/" + key + ".msg"))) {
+                String content = br.readLine();
+                if (content != null){
+                    return content;
+                }
+            }
+        }catch (Exception e) {
+            //dosya yoksa RAM'den oku
+        }
+
         String value = CommandParser.messages.get(key);
         if (value != null) {
             return value;
